537455ebe16f499ef2e98f20ef74659a742ca489
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-05-13 11:10:07 +0000

    started cleaning up community plugin repository widget
    git-svn-id: http://code.elgg.org@6026 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/widgets/plugins/view.php
+++ views/default/widgets/plugins/view.php
@@ -1,67 +1,36 @@
-<script type="text/javascript">
-$(document).ready(function () {
-
-$('a.show_file_desc').click(function () {
-	$(this.parentNode).children("[class=filerepo_listview_desc]").slideToggle("fast");
-	return false;
-});
-
-}); /* end document ready function */
-</script>
-
-
 <?php
 
-    //the page owner
-	$owner = $vars['entity']->owner_guid;
-	
-	//the number of files to display
-	$number = (int) $vars['entity']->num_display;
-	if (!$number)
-		$number = 1;
-	
-	//get the layout view which is set by the user in the edit panel
-	//$get_view = (int) $vars['entity']->gallery_list;
-	//if (!$get_view || $get_view == 1) {
-	//    $view = "list";
-    //}else{
-    //    $view = "list";
-   // }
+//the page owner
+$owner = $vars['entity']->owner_guid;
 
-	//get the user's files
-	$files = get_user_objects($vars['entity']->owner_guid, "plugin_project", $number, 0);
-	
-	//if there are some files, go get them
-	if ($files) {
-    	
-    	    echo "<div id=\"pluginsrepo_widget_layout\">";
-                	    
-            //display in list mode
-            foreach($files as $f){
-          
-                $mime = $f->mimetype;
-                echo "<div class=\"pluginsrepo_widget_singleitem\">";
-            	echo "<div class=\"pluginsrepo_listview_icon\"><a href=\"{$f->getURL()}\"><img src=\"http://community.elgg.org/mod/community_plugins/graphics/icons/archive.gif\"></a></div>";
-            	echo "<div class=\"pluginsrepo_widget_content\">";
-            	echo "<div class=\"pluginsrepo_listview_title\"><p class=\"filerepo_title\">" . $f->title . "</p></div>";
-            	echo "<div class=\"pluginsrepo_listview_date\"><p class=\"filerepo_timestamp\"><small>" . friendly_time($f->time_created) . "</small></p></div>";
-            	//$description = $f->description;
-		//        if (!empty($description)) echo "<a href=\"javascript:void(0);\" class=\"show_file_desc\">". elgg_echo('more') ."</a><br /><div class=\"filerepo_listview_desc\">" . $description . "</div>";
-             echo "</div><div class=\"clearfloat\"></div></div>";
-            				
-        	}
-        	    
-        //get a link to the users files
-        $users_file_url = $vars['url'] . "pg/plugins/" . get_user($f->owner_guid)->username;
-        	
-        echo "<div class=\"pluginsrepo_widget_singleitem_more\"><a href=\"{$users_file_url}\">" . elgg_echo('plugins:more') . "</a></div>";
-        echo "</div>";
-        	
-				
-	} else {
-		
-		echo "<div class=\"contentWrapper\">" . elgg_echo("plugins:none") . "</div>";
-		
+//the number of files to display
+$number = (int) $vars['entity']->num_display;
+if (!$number) {
+	$number = 4;
+}
+
+//get the user's plugin projects
+$plugins = get_user_objects($vars['entity']->owner_guid, "plugin_project", $number, 0);
+
+if ($plugins) {
+	echo "<div id=\"pluginsrepo_widget_layout\">";
+	//display in list mode
+	foreach($plugins as $plugin) {
+		echo "<div class=\"pluginsrepo_widget_singleitem\">";
+		echo "<div class=\"pluginsrepo_listview_icon\"><a href=\"{$plugin->getURL()}\"><img src=\"http://community.elgg.org/mod/community_plugins/graphics/icons/archive.gif\"></a></div>";
+		echo "<div class=\"pluginsrepo_widget_content\">";
+		echo "<div class=\"pluginsrepo_listview_title\"><p class=\"filerepo_title\">" . $plugin->title . "</p></div>";
+		echo "<div class=\"pluginsrepo_listview_date\"><p class=\"filerepo_timestamp\"><small>" . friendly_time($plugin->time_created) . "</small></p></div>";
+		echo "</div><div class=\"clearfloat\"></div></div>";
+
 	}
 
-?>
+	//get a link to the user's plugins
+	$users_file_url = $vars['url'] . "pg/plugins/" . get_user($f->owner_guid)->username;
+
+	echo "<div class=\"pluginsrepo_widget_singleitem_more\"><a href=\"{$users_file_url}\">" . elgg_echo('plugins:more') . "</a></div>";
+	echo "</div>";
+
+} else {
+	echo "<div class=\"contentWrapper\">" . elgg_echo("plugins:none") . "</div>";
+}
