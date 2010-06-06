1b6800f4d7e099d3d816f12a28b3eda804a85bdd
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-06 13:24:10 +0000

    added a rough download graph to stats - this needs to be cleaned up
    git-svn-id: http://code.elgg.org@6371 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/plugins/admin/stats.php
+++ views/default/plugins/admin/stats.php
@@ -1,1 +1,29 @@
+<h3>Plugin download trends</h3>
+
+<script type="text/javascript" src="<?php echo $vars['url']; ?>mod/community_plugins/vendors/flot/jquery.flot.js"></script>
+
 <?php
+// this needs to be moved into a function
+$start_date = time() - 30 * 3600 * 24;
+$downloads = get_annotations(0, 'object', 'plugin_project', 'download', '', 0, 9999999, 0, 'asc', $start_date);
+$histogram = array_fill(0, 30, 0);
+foreach ($downloads as $download) {
+	$day = (int)floor(($download->time_created - $start_date) / (3600 * 24));
+	$histogram[$day]++;
+}
+$plot_string = '';
+foreach ($histogram as $k=>$v) {
+	$plot_string .= "[$k, $v],";
+}
+$plot_string = rtrim($plot_string, ',');
+?>
+<div id="plugins_download_plot"></div>
+
+<script language="javascript" type="text/javascript">
+$(function () {
+    var downloads = [<?php echo $plot_string; ?>];
+
+    $.plot($("#plugins_download_plot"), [downloads]);
+});
+</script>
+
--- views/default/plugins/css.php
+++ views/default/plugins/css.php
@@ -210,7 +210,11 @@
 */
 
 
-
+#plugins_download_plot {
+width: 640px;
+height: 320px;
+margin: 20px;
+}
 
 
 
