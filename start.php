f0c832028c68605fa072f70e75c66e69c2177f9f
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-13 11:13:55 +0000

    added skeleton for community groups overrides

---
+++ start.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Overrides for the groups plugin on Elgg community site
+ * 
+ */
+
+register_elgg_event_handler('init', 'system', 'community_groups_init');
+
+function community_groups_init() {
+	global $CONFIG;
+
+}
+
