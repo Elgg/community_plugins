ef0d384abf86bbd792739cd43c399878c74aa082
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-07-25 17:19:28 +0000

    added reported content to plugin pages
    git-svn-id: http://code.elgg.org@6776 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/plugins/project_sidebar.php
+++ views/default/plugins/project_sidebar.php
@@ -21,3 +21,13 @@ echo elgg_view('plugins/project_sidebar/images', array('entity' => $project));
 
 echo elgg_view('plugins/project_sidebar/other', array('entity' => $project));
 
+// add reported content so users can report bad plugins
+// @todo Elgg 1.8 moves reported content to footer so this won't be needed
+if (isloggedin()) {
+	if (elgg_view_exists('reportedcontent/owner_block')) {
+		echo '<div class="sidebarBox">';
+		echo elgg_view('reportedcontent/owner_block');
+		echo '</div>';
+	}
+}
+
