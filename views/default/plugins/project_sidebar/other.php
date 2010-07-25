7d1a1deb67c7171de88f46ab21ef49bfb7a4d55f
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-07-25 12:56:37 +0000

    updated other plugins view to use elgg_get_entities()
    git-svn-id: http://code.elgg.org@6768 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/plugins/project_sidebar/other.php
+++ views/default/plugins/project_sidebar/other.php
@@ -2,7 +2,13 @@
 
 $project = $vars['entity'];
 
-$all_user_plugins = get_entities("object", "plugin_project", $project->owner_guid,"", 99, 0);
+$params = array(
+	'types' => 'object',
+	'subtypes' => 'plugin_project',
+	'owner_guid' => $project->owner_guid,
+	'limit' => 0,
+);
+$all_user_plugins = elgg_get_entities($params);
 
 if (count($all_user_plugins) > 1) {
 ?>
@@ -10,21 +16,21 @@ if (count($all_user_plugins) > 1) {
 	<h3>Other Projects</h3>
 	<div class="contentWrapper">
 		<p><?php echo page_owner_entity()->name; ?>'s plugins:</p>
-	<?php
-		if($all_user_plugins){
+<?php
+		if ($all_user_plugins) {
 			echo "<select class='choose_plugin' onchange=\"window.open(this.options[this.selectedIndex].value,'_top')\">";
-			foreach($all_user_plugins as $up){
-				if(get_input('guid') == $up->guid)
+			foreach ($all_user_plugins as $up) {
+				if (get_input('guid') == $up->guid) {
 					$selected = "SELECTED";
-				else
+				} else {
 					$selected = '';
+				}
 				echo "<option value=\"{$up->getURL()}\" $selected>{$up->title}</option>";
 			}
 			echo "</select>";
 		}
-	?>
+?>
 	</div>
 </div>
 <?php
 }
-?>
