29322d7d33e3e125ca13d26ff81e56211b356763
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-07-25 19:54:35 +0000

    added first version of utility to combine two plugin projects
    git-svn-id: http://code.elgg.org@6782 36083f99-b078-4883-b0ff-0f9b5a30f544

---
+++ actions/admin/combine.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Action for combining two plugin projects
+ */
+
+global $CONFIG;
+
+$old_guid = (int)get_input('old_guid');
+$new_guid = (int)get_input('new_guid');
+
+$old_project = get_entity($old_guid);
+$new_project = get_entity($new_guid);
+
+if (!($old_project instanceof PluginProject) ||
+	!($new_project instanceof PluginProject)) {
+	register_error('The GUIDs must be for 2 plugin projects');
+	forward(REFERER);
+}
+
+$old_name = $old_project->title;
+
+// move releases for the old project to the new project
+$params = array(
+	'types' => 'object',
+	'subtypes' => 'plugin_release',
+	'container_guids' => $old_project->guid,
+	'limit' => 0,
+);
+$releases = elgg_get_entities($params);
+foreach ($releases as $release) {
+	$release->container_guid = $new_project->guid;
+	$release->save();
+}
+
+// move download count to new project
+$annotation_name = get_metastring_id('download', TRUE);
+$query = "UPDATE {$CONFIG->dbprefix}annotations
+	SET entity_guid=$new_project->guid
+	WHERE entity_guid=$old_project->guid AND name_id=$annotation_name";
+update_data($query);
+
+system_message("$old_name has been combined into the project $new_project->title");
+forward(REFERER);
---
+++ views/default/plugins/admin/utilities/combine.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Combining plugin projects
+ */
+?>
+<h3>Combine plugin projects</h3>
+<?php
+$form_body = '<p>';
+$form_body .= elgg_view('input/text',
+		array(
+			'internalname' => 'old_guid',
+			'class' => 'none',
+		));
+$form_body .= ' GUID of plugin project that is be replaced in the combination';
+$form_body .= '</p><p>';
+
+$form_body .= elgg_view('input/text',
+		array(
+			'internalname' => 'new_guid',
+			'class' => 'none',
+		));
+$form_body .= ' GUID of plugin project that remains in the combination';
+$form_body .= '</p>';
+
+$form_body .= elgg_view('input/submit', array('value' => elgg_echo('submit')));
+
+$action = "{$vars['url']}action/plugins/combine/";
+
+echo elgg_view('input/form', array('body' => $form_body, 'action' => $action));
