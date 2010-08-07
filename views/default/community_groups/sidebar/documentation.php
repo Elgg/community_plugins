37f33f5aa42afed90ec2e5654a01aa0e8e6efa05
Cash Costello <cash@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-08-07 20:43:52 +0000

    missed this from previous commit

--- views/default/community_groups/sidebar/documentation.php
+++ views/default/community_groups/sidebar/documentation.php
@@ -1,6 +1,6 @@
 <div class="sidebarBox">
 	<h3><?php echo elgg_echo("Documentation"); ?></h3>
-	<div class="contentWrapper">
+	<div class="sidebarWrapper">
 		<ul id="documentation_links">
 			<li><a href="http://docs.elgg.org/wiki/Install_Troubleshooting">Install Troubleshooting</a></li>
 			<li><a href="http://docs.elgg.org/wiki/Category:Administration_FAQ">Using Elgg FAQ</a></li>
