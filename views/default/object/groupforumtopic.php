5e4572ec9c14ac119f1a2bc7c85056b9fdcddcf1
Brett Profitt <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-08-30 01:37:37 +0000

    Using user avatars instead of group for forum object listings

--- views/default/object/groupforumtopic.php
+++ views/default/object/groupforumtopic.php
@@ -28,7 +28,7 @@ if (get_context() == "search") {
 	}
 	$info .= '</span></p>';
 	//get the group avatar
-	$icon = elgg_view("profile/icon",array('entity' => $group, 'size' => 'small'));
+	$icon = elgg_view("profile/icon", array('entity' => $u, 'size' => 'tiny'));
 	//get the group and topic title
 	if ($group instanceof ElggGroup) {
 		$info .= "<p>" . elgg_echo('group') . ": <a href=\"{$group->getURL()}\">".htmlentities($group->name, ENT_QUOTES, 'UTF-8') ."</a></p>";
