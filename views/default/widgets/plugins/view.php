ea5992473ccbf108f9cc4f7f76671810b3712a34
Brett Profitt <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-09-04 17:18:27 +0000

    Fixes #2448: Using correctly variable to get username on plugins widget.
    git-svn-id: http://code.elgg.org@6906 36083f99-b078-4883-b0ff-0f9b5a30f544

--- views/default/widgets/plugins/view.php
+++ views/default/widgets/plugins/view.php
@@ -2,6 +2,7 @@
 
 //the page owner
 $owner = $vars['entity']->owner_guid;
+$owner_entity = get_entity($owner);
 
 //the number of files to display
 $number = (int) $vars['entity']->num_display;
@@ -20,7 +21,7 @@ if ($plugins) {
 	}
 
 	//get a link to the user's plugins
-	$users_file_url = $vars['url'] . "pg/plugins/developer/" . get_user($f->owner_guid)->username;
+	$users_file_url = $vars['url'] . "pg/plugins/developer/" . $owner_entity->username;
 
 	echo "<div class=\"pluginsrepo_widget_singleitem_more\"><a href=\"{$users_file_url}\">" . elgg_echo('plugins:more') . "</a></div>";
 	echo "</div>";
