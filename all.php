df9d5e833241004a326513f0f2b7a0ca56343685
Cash Costello <cash.costello@gmail.com>
2011-11-15 12:59:34 -0500

    encourage people to update bookmarks so that we can eventually get rid of these files

--- all.php
+++ all.php
@@ -8,4 +8,6 @@
 
 require_once(dirname(dirname(dirname(__FILE__))) . "/engine/start.php");
 
+system_message('Please update your bookmark or report this link to the site owner as this page has moved.');
+
 include dirname(__FILE__) . "/pages/plugins/all.php";
