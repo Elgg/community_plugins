607e57a444fde81d04fd24b19cf0d55b3cef6619
Cash Costello <cash.costello@gmail.com>
2012-01-10 08:07:31 -0500

    moved community search css into main css view

--- views/default/plugins/metatags.php
+++ views/default/plugins/metatags.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="<?php echo $vars['url']; ?>mod/community_plugins/_css/community_plugins.css" media="screen" />
+
 <link rel="stylesheet" type="text/css" href="<?php echo $vars['url'];?>mod/community_plugins/js/dropdown-check-list.1.0/ui.dropdownchecklist.standalone.css" />
 <script type="text/javascript" src="<?php echo $vars['url'];?>mod/community_plugins/js/dropdown-check-list.1.0/ui.dropdownchecklist.js"></script>
 <script type="text/javascript" src="<?php echo $vars['url']; ?>mod/community_plugins/js/community_plugins.js"></script>
