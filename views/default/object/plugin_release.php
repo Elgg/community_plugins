5856791cbba7d7fe47c141bae245eddf074bc514
Evan Winslow <evan@elgg.org>
2012-06-25 23:51:34 -0700

    Plugin release notes have normal default formatting now.

--- views/default/object/plugin_release.php
+++ views/default/object/plugin_release.php
@@ -8,7 +8,7 @@ $release = $vars['entity'];
 $notes = $release->release_notes;
 
 if ($notes) {
-	echo "<div class=\"pluginsrepo_description\">";
+	echo "<div class=\"elgg-output\">";
 	echo "<h3>Release notes:</h3>";
 	echo autop($notes);
 	echo "</div>";
