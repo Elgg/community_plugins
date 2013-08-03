ab6931fe534d446af80606085f7deee7cf78f329
Matt Beckett <beck24@gmail.com>
2013-08-03 03:50:51 -0600

    uses default elgg lightbox for screenshots, removes more superfluous css

--- views/default/plugins/css.php
+++ views/default/plugins/css.php
@@ -47,7 +47,7 @@
 }
 
 
-.plugin-screenshots > li {
+.elgg-plugin-screenshots > li {
 	margin-left: 2px;
 }
 
@@ -70,111 +70,6 @@ span.downloadsnumber {
 }
 
 
-/**
- * jQuery lightBox plugin
- * This jQuery plugin was inspired and based on Lightbox 2 by Lokesh Dhakar (http://www.huddletogether.com/projects/lightbox2/)
- * and adapted to me for use like a plugin from jQuery.
- * @name jquery-lightbox-0.5.css
- * @author Leandro Vieira Pinho - http://leandrovieira.com
- * @version 0.5
- * @date April 11, 2008
- * @category jQuery plugin
- * @copyright (c) 2008 Leandro Vieira Pinho (leandrovieira.com)
- * @license CC Attribution-No Derivative Works 2.5 Brazil - http://creativecommons.org/licenses/by-nd/2.5/br/deed.en_US
- * @example Visit http://leandrovieira.com/projects/jquery/lightbox/ for more informations about this jQuery plugin
- */
-#jquery-overlay {
-	position: absolute;
-	top: 0;
-	left: 0;
-	z-index: 90;
-	width: 100%;
-	height: 500px;
-}
-#jquery-lightbox {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	z-index: 100;
-	text-align: center;
-	line-height: 0;
-}
-#jquery-lightbox a img { border: none; }
-#lightbox-container-image-box {
-	position: relative;
-	background-color: #fff;
-	width: 250px;
-	height: 250px;
-	margin: 0 auto;
-}
-#lightbox-container-image { padding: 10px; }
-#lightbox-loading {
-	position: absolute;
-	top: 40%;
-	left: 0%;
-	height: 25%;
-	width: 100%;
-	text-align: center;
-	line-height: 0;
-}
-#lightbox-nav {
-	position: absolute;
-	top: 0;
-	left: 0;
-	height: 100%;
-	width: 100%;
-	z-index: 10;
-}
-
-#lightbox-container-image-box > #lightbox-nav { left: 0; }
-
-#lightbox-nav a { outline: none;}
-
-#lightbox-nav-btnPrev, #lightbox-nav-btnNext {
-	width: 49%;
-	height: 100%;
-	zoom: 1;
-	display: block;
-}
-#lightbox-nav-btnPrev {
-	left: 0;
-	float: left;
-}
-#lightbox-nav-btnNext {
-	right: 0;
-	float: right;
-}
-#lightbox-container-image-data-box {
-	font: 10px Verdana, Helvetica, sans-serif;
-	background-color: #fff;
-	margin: 0 auto;
-	line-height: 1.4em;
-	overflow: auto;
-	width: 100%;
-	padding: 0 10px 0;
-}
-#lightbox-container-image-data {
-	padding: 0 10px;
-	color: #666;
-}
-#lightbox-container-image-data #lightbox-image-details {
-	width: 70%;
-	float: left;
-	text-align: left;
-}
-#lightbox-image-details-caption { font-weight: bold; }
-#lightbox-image-details-currentNumber {
-	display: block;
-	clear: left;
-	padding-bottom: 1.0em;
-}
-#lightbox-secNav-btnClose {
-	width: 66px;
-	float: right;
-	padding-bottom: 0.7em;
-}
-
 
 /***************** Entity listing ********************/
 
@@ -186,6 +81,51 @@ span.info_item {
 span.info_item img {
 	padding-right: 2px;
 	vertical-align: middle;
+}
+
+
+/***************** Filters ********************/
+
+div#plugin_filters select.input-select {
+    width: 222px;
+}
+
+div#plugin_filters input.input-text {
+    width: 210px;
+}
+
+div#plugin_filters div.filter_fields {
+    padding-bottom: 10px;
+}
+
+div#plugin_filters div.filter_label {
+    color: #555;
+    margin-bottom: 5px;
+    font-weight: normal;
+}
+
+div#plugin_filters div.filter_label.ui-state-active {
+    font-weight: bold;
+}
+
+
+div#plugin_filters button.clear_search {
+    background: none repeat scroll 0 0 #4690d6;
+    border: medium none;
+    border-radius: 5px 5px 5px 5px;
+    color: white;
+    cursor: pointer;
+    font: bold 12px/100% Arial,Helvetica,sans-serif;
+    height: 25px;
+    margin: 10px 0;
+    padding: 2px 6px;
+    width: auto;
+    float: right;
+}
+
+div#plugin_filters button.clear_search:hover {
+    background: none repeat scroll 0 0 #0054a7;
+    color: white;
 }
 
 
